2ccfe328a856dac00a03677338eb6fdb4540d6bf
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-11-04 07:19:50 +0600

    display all data

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,28 @@
+import { useLoaderData } from "react-router-dom";
 import Banner from "../../components/Banner/Banner";
+import Card from "../../components/Card/Card";
 
 const Home = () => {
-    return (
+  const products = useLoaderData();
+  console.log(products);
+  return (
+    <div>
+      <Banner />
+      <div className="text-center">
+        <h1 className="text-4xl my-5 font-bold text-[#0B0B0B]">
+          Explore Cutting-Edge Gadgets
+        </h1>
         <div>
-            <Banner/>
+          <div></div>
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 my-10">
+            {products.map((product) => (
+              <Card key={product.product_id} product={product} />
+            ))}
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,14 @@
+const Card = ({ product }) => {
+  console.log(product);
+  const { product_image, product_title, price } = product;
+  return (
+    <div className="border p-5 rounded-lg text-left bg-gray-100">
+      <img className="mx-auto bg-white px-16 py-10 rounded-lg h-56 w-full" src={product_image} alt="" />
+      <h3 className="font-bold text-xl mt-5">{product_title}</h3>
+      <p className="font-medium text-gray-500 mt-2">Price: ${price}</p>
+      <button className="py-3 px-5 rounded-full border border-[#9538E2] text-[#9538E2] font-semibold mt-2">View Details</button>
+    </div>
+  );
+};
+
+export default Card;
