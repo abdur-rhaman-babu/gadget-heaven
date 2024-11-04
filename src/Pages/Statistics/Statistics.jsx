591b2798e830f10a802aea3b875541c8100f61b2
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-11-04 06:49:14 +0600

    create json data

--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+import ReactStars from "react-rating-stars-component";
 const Statistics = () => {
-    return (
-        <div>
-            <h1>Statistics</h1>
-        </div>
-    );
+const [star,setStar] = useState('')
+  const ratingChanged = (newRating) => {
+    setStar(newRating)
+  };
+  return (
+    <div>
+      <ReactStars
+        count={5}
+        onChange={ratingChanged}
+        size={24}
+        activeColor="#ffd700"
+      />
+      <p>{star}</p>
+    </div>
+  );
 };
 
 export default Statistics;
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,14 +1,15 @@
+import { NavLink } from 'react-router-dom';
 import banner from '../../assets/image/banner.jpg'
 const Banner = () => {
   return (
    <div>
-     <div className="bg-[#9538E2] bg-cover rounded-b-2xl -z-50 bg-center text-center text-white pt-5 px-32">
+     <div className="bg-[#9538E2] bg-cover rounded-b-2xl -z-50 bg-center text-center px-4 text-white pt-5 lg:px-32">
       <h1 className="md:text-5xl text-3xl font-bold text-center my-5">Upgrade Your Tech Accessorize with Gadget Heaven Accessories</h1>
-      <p className="px-48 text-center my-5">
+      <p className="lg:px-48 text-center my-5">
         Explore the latest gadgets that will take your experience to the next
         level. From smart devices to the coolest accessories, we have it all!
       </p>
-      <button className="text-xl mb-48 font-bold bg-white text-[#9538E2] py-3 px-5 rounded-full">Shop Now</button>
+      <NavLink to='/dashboard'><button className="text-xl mb-48 font-bold bg-white text-[#9538E2] py-3 px-5 rounded-full">Shop Now</button></NavLink>
     </div>
       <img className='h-[400px] w-[700px] mx-auto -mt-36 mb-10 rounded-2xl bg-[rgba(255,255,255,0.3)] p-4 border border-white' src={banner} alt="" />
    </div>
